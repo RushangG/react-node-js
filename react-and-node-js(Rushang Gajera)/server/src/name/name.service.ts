4edb2536227bd7bf3b5319d54a120fac945472b3
Rushang Gajera <gajerarushang17@gmail.com>
2026-09-16 18:05:49 +0530

    user update

--- react-and-node-js(Rushang Gajera)/server/src/name/name.service.ts
+++ react-and-node-js(Rushang Gajera)/server/src/name/name.service.ts
@@ -1,9 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateNameDto } from './dto/create-name.dto';
 import { UpdateNameDto } from './dto/update-name.dto';
-
+import { Name } from './entities/name.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { throwError } from 'rxjs';
 @Injectable()
 export class NameService {
+  constructor(
+    @InjectRepository(Name)
+    private nameRepo: Repository<Name>,
+  ) {}
+
   private Names: CreateNameDto[] = [];
   private id: number = 1;
   create(createNameDto: CreateNameDto) {
@@ -14,16 +22,27 @@ export class NameService {
   }
 
   findAll() {
-    return this.Names;
+    let names = this.nameRepo.find();
+    return names;
+
+    // basic array method.
+    // return this.Names;
   }
 
-  findOne(id: number) {
-    let Name = this.Names.find((name) => name.id === id);
-    if (!Name) {
+  async findOne(id: number) {
+    let name = await this.nameRepo.findOneBy({ id });
+
+    if (!name) {
       throw new NotFoundException(`Not found Name`);
     }
+    return name;
 
-    return Name;
+    // let Name = this.Names.find((name) => name.id === id);
+    // if (!Name) {
+    //   throw new NotFoundException(`Not found Name`);
+    // }
+
+    // return Name;
   }
 
   update(id: number, updateNameDto: UpdateNameDto) {
@@ -39,15 +58,25 @@ export class NameService {
     return this.Names[indexName];
   }
 
-  remove(id: number) {
-    let indexName = this.Names.findIndex((name) => name.id === id);
+  async remove(id: number) {
+    let name = await this.nameRepo.findOneBy({ id });
 
-    if (indexName !== -1) {
-      this.Names.splice(indexName, 1);
-    } else {
-      throw new NotFoundException(`Not found Name`);
+    if (!name) {
+      throw new NotFoundException(`Not found Name for delete`);
     }
 
-    return `This action removes a #${id} name`;
+    await this.nameRepo.delete({ id });
+
+    return `delete name succesfully`;
+
+    // let indexName = this.Names.findIndex((name) => name.id === id);
+
+    // if (indexName !== -1) {
+    //   this.Names.splice(indexName, 1);
+    // } else {
+    //   throw new NotFoundException(`Not found Name`);
+    // }
+
+    // return `This action removes a #${id} name`;
   }
 }
